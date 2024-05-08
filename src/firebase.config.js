3e640a354bf868572e8b4a01b54b832abb20d684
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-05-08 09:57:04 +0600

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAjB6NxZs4_fTMUNELE9vFPPIRU7d_mcng",
+    authDomain: "solo-sphere-m11.firebaseapp.com",
+    projectId: "solo-sphere-m11",
+    storageBucket: "solo-sphere-m11.appspot.com",
+    messagingSenderId: "413932630598",
+    appId: "1:413932630598:web:1b122764de56565263d047"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ src/layout/Main.jsx
@@ -0,0 +1,17 @@
+
+import Navbar from '../components/Navbar';
+
+const Main = () => {
+    return (
+        <div>
+            {/* navbar */}
+            <Navbar></Navbar>
+        
+            {/* Outlet */}
+
+            {/* footer */}
+        </div>
+    );
+};
+
+export default Main;
