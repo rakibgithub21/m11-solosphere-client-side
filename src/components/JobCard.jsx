7a7135cb40f4504d3977c714859e8bfaf2141973
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-05-08 18:05:48 +0600

    making job details page

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,12 +1,15 @@
 /* eslint-disable react/prop-types */
 
+import { Link } from "react-router-dom"
+
 const JobCard = ({ job }) => {
-    const{category} = job
+    console.log(job);
+    const { _id, category, deadline, description, job_title, max_price, min_price } = job
     return (
-        <div className='w-full max-w-sm px-4 py-3 bg-white rounded-md shadow-md hover:scale-[1.05] transition-all'>
+        <Link to={`/job/${_id}`} className='w-full max-w-sm px-4 py-3 bg-white rounded-md shadow-md hover:scale-[1.05] transition-all'>
             <div className='flex items-center justify-between'>
                 <span className='text-xs font-light text-gray-800 '>
-                    Deadline: 20/12/2024
+                    Deadline: {deadline}
                 </span>
                 <span className='px-3 py-1 text-[8px] text-blue-800 uppercase bg-blue-200 rounded-full '>
                     {category}
@@ -15,17 +18,17 @@ const JobCard = ({ job }) => {
 
             <div>
                 <h1 className='mt-2 text-lg font-semibold text-gray-800 '>
-                    Build Dynamic Website Using React
+                    {job_title}
                 </h1>
 
-                <p className='mt-2 text-sm text-gray-600 '>
-                    Lorem ipsum dolor sit adipisicing elit...
+                <p title={description} className='mt-2 text-sm text-gray-600 '>
+                    {description.substring(0,70)}....
                 </p>
                 <p className='mt-2 text-sm font-bold text-gray-600 '>
-                    Range: $100 - $150
+                    Range: ${max_price} - ${min_price}
                 </p>
             </div>
-        </div>
+        </Link>
     )
 }
 
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,6 +3,7 @@ import Main from "../layout/Main";
 import Home from "../pages/Home";
 import Login from "../pages/Authentication/Login";
 import Register from "../pages/Authentication/Register";
+import JobDetails from "../pages/JobDetails";
 
 export const router = createBrowserRouter([
     {
@@ -21,6 +22,11 @@ export const router = createBrowserRouter([
             {
                 path: '/register',
                 element:<Register></Register>
+            },
+            {
+                path: '/job/:id',
+                element: <JobDetails></JobDetails>,
+                loader: ({ params }) => fetch(`${import.meta.env.VITE_API_URL}/job/${params.id}`)
             },
         ]
     }
