07ae5c59a80a4448b5ee948664139ba8d5e4f21e
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-05-09 08:41:30 +0600

    make bid request page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -10,6 +10,7 @@ import MyPostedJobs from "../pages/MyPostedJobs";
 import UpdateJob from "../pages/UpdateJob";
 import PrivateRoute from "./PrivateRoute";
 import MyBids from "../pages/MyBids";
+import BidRequests from "../pages/BidRequests";
 
 export const router = createBrowserRouter([
     {
@@ -52,6 +53,10 @@ export const router = createBrowserRouter([
             {
                 path: '/my-bids',
                 element: <PrivateRoute><MyBids></MyBids></PrivateRoute>
+            },
+            {
+                path: '/bid-request',
+                element: <PrivateRoute><BidRequests></BidRequests></PrivateRoute>
             },
         ]
     }
