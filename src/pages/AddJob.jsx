f10b146de13425f9ff6b15ec2eace2f52c42aae7
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-05-09 16:32:20 +0600

    make a hook component for auth context

--- src/pages/AddJob.jsx
+++ src/pages/AddJob.jsx
@@ -1,13 +1,13 @@
-import { useContext, useState } from "react";
+import { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { AuthContext } from "../provider/AuthProvider";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import axios from "axios";
+import useAuth from "../hooks/useAuth";
 
 const AddJob = () => {
-    const{user}= useContext(AuthContext)
+    const{user} = useAuth()
     const [startDate, setStartDate] = useState(new Date());
     const navigate = useNavigate()
 
